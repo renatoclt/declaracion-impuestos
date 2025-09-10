317e556ec249b3261c0d3bf71169f6bd6d38fbaa
Renato Jesus Cohaila La Torre <rcohaila@emeal.nttdata.com>
2025-09-10 13:09:32 -0500

    instalcion de bootstrap, material , ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideEffects()
+]
 };
