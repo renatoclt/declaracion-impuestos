da2dd5f0323997aa9c2f2bf77cacd6fab0773547
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2025-09-21 20:06:25 -0500

    add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 import { UserDashboard } from './pages/user-dashboard/user-dashboard';
+import { authGuard } from './shared/guard/auth.guard';
 
 export const routes: Routes = [
   {
@@ -10,14 +11,21 @@ export const routes: Routes = [
   },
   {
     path: 'admin-dashboard',
-    component: AdminDashboard
+    component: AdminDashboard,
+    canActivate: [authGuard]
   },
   {
     path: 'user-dashboard',
-    component: UserDashboard
+    component: UserDashboard,
+    canActivate: [authGuard]
   },
-   {
+  {
     path: 'declaration',
-    loadChildren: () => import('./pages/declaration/declaration.routes').then(m => m.declaration)
+    loadChildren: () => import('./pages/declaration/declaration.routes').then(m => m.declaration),
+    canActivate: [authGuard]
   },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
---
+++ src/app/shared/guard/auth.guard.ts
@@ -0,0 +1,26 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+    const router = inject(Router);
+
+    const token = localStorage.getItem('auth_token');
+    const role = localStorage.getItem('user_role');
+
+    if (!token) {
+        router.navigate(['/login']);
+        return false;
+    }
+
+    if (role === 'admin' && state.url !== '/admin-dashboard') {
+        router.navigate(['/admin-dashboard']);
+        return false;
+    }
+
+    if (role === 'taxpayer' && state.url !== '/user-dashboard') {
+        router.navigate(['/user-dashboard']);
+        return false;
+    }
+
+    return true;
+};
