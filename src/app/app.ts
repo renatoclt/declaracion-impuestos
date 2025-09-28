f854dd2ec85e8c5cd1d54635e5e1db2778b2d109
renato <renatoclt@hotmail.com>
2025-09-28 11:52:32 -0500

    cambio de menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { AuthService } from './shared/services/auth-service';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Cabecera } from './pages/cabecera/cabecera';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, Cabecera],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-
-  mostrarCabeceraEnComponente:boolean = true;
+export class App implements OnInit {
+  private AuthService = inject(AuthService);
+  mostrarCabeceraEnComponente:boolean = false;
   protected readonly title = signal('declaracion-impuestos');
-  
+  ngOnInit(): void {
+    if (this.AuthService.getUserRole()){
+      this.mostrarCabeceraEnComponente = true;
+    }
+
+  }
 }
--- src/app/pages/cabecera/cabecera.ts
+++ src/app/pages/cabecera/cabecera.ts
@@ -1,16 +1,22 @@
-import { Component, input, Input, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, input, Input, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '@/app/shared/services/auth-service';
 import { User } from '@/app/shared/interfaces/user.interface';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-cabecera',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './cabecera.html',
 })
 export class Cabecera  implements OnInit{
-    
+    private readonly router = inject(Router);
+    currentUser: User | null = null;
+    private readonly http = inject(HttpClient);
+    private readonly authService = inject(AuthService);
+    private readonly API_URL = 'http://localhost:3000';
     @Input() mostrarCebecera: boolean = true;
     @Input() loggedInUser : User | null = null;
 
@@ -18,11 +24,57 @@ export class Cabecera  implements OnInit{
 
     ngOnInit(): void {
       //this.loggedInUser = this.authservice.login();
+      const requests = {
+        users: this.getUsers(),
+      }
     }
 
     logout():void{
       this.authservice.logout();
       this.loggedInUser = null;
+    }
+    createNewDeclaration() {
+    this.router.navigate(['/declarations/new']);
+  }
+
+  editDeclaration(id: number) {
+    this.router.navigate(['/declarations/edit', id]);
+  }
+
+  viewDeclaration(id: number) {
+    this.router.navigate(['/declarations/view', id]);
+  }
+
+  viewIncomes() {
+    this.router.navigate(['/declaration/income']);
+  }
+
+  onDeclarationIncome() {
+    this.router.navigate(['/declaration/income']);
+  }
+
+  onDeclarationExpense() {
+    this.router.navigate(['/declaration/expense']);
+  }
+
+  onCalculation() {
+    this.router.navigate(['/declaration/calculation']);
+  }
+
+  onHistory() {
+    this.router.navigate(['/declaration/history']);
+  }
+
+  viewExpenses() {
+    this.router.navigate(['declaration/expense']);
+  }
+
+  viewAllHistory() {
+    this.router.navigate(['/declarations/history']);
+  }
+
+  private getUsers(): Observable<User[]> {
+      return this.http.get<User[]>(`${this.API_URL}/users`);
     }
 
 }
